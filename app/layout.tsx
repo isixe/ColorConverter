364b808c52f2b7093a702bf509ea01f70fa8a296
isixe <isixe@outlook.com>
2025-04-26 18:59:29 +0800

    🎈 perf(app): add keywords, canonical and openGraph metadata for layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,32 @@ import Footer from '@/components/layout/footer'
 import Header from '@/components/layout/header'
 import '@/styles/global.css'
 import type { Metadata } from 'next'
+import { headers } from 'next/headers'
 
-export const metadata: Metadata = {
-  title: 'ColorConverter',
-  description: 'A Color Converter Tool'
+export async function generateMetadata(): Promise<Metadata> {
+  const headersList = await headers()
+  const protocol = headersList.get('x-forwarded-proto') || 'http'
+  const host = headersList.get('host') || 'localhost:3000'
+  const url = `${protocol}://${host}`
+
+  return {
+    title: 'ColorConverter',
+    keywords:
+      'color, color, color converter, color tool, color generator, color format',
+    description:
+      'A color conversion tool that supports conversions between RGB, HEX, HSL, HWB, and CMYK color formats.',
+    alternates: {
+      canonical: url
+    },
+    openGraph: {
+      title: 'ColorConverter',
+      description:
+        'A color conversion tool that supports conversions between RGB, HEX, HSL, HWB, and CMYK color formats.',
+      url,
+      siteName: 'ColorConverter',
+      locale: 'en'
+    }
+  }
 }
 
 export default function RootLayout({
